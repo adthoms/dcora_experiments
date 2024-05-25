33d7e09a0c0b79293429aa0bae9765793f640ea5
Jared Velasquez <jaredvel25@gmail.com>
2024-05-24 22:23:58 -0700

    Implemented pyfg_to_tum for parsing ground truth from pyfg file

---
+++ pipeline.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e  # exit on error
+
+if [[ "$#" -ne 3 ]]; then
+    echo "Usage: [.pyfg file] [(CORA) .tum file] [(DCORA) .tum file]"
+    exit 1
+fi
+
+# printf "\033c" resets the output
+function log { printf "\033c"; echo -e "\033[32m[$BASH_SOURCE] $1\033[0m"; }
+function echo_and_run { echo -e "\$ $@" ; read input; "$@" ; read input; }
+
+# always run in script directory
+# parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+# cd "$parent_path"
+
+log "Generate ground truth from .pyfg file"
+
+log "Compare TUM files"
+echo_and_run evo_ape TUM_FILE_CORA TUM_FILE_DCORA -va --plot --plot_mode xz --save_results pipeline.zip
